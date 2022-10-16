07771a7a31dcc64158c6eca5f7b51640ce06812a
User <kenjaxojayevnurulloxon@gmail.com>
2022-10-16 15:15:46 +0500

    Remove

--- app/src/main/java/com/example/githubexample/MainActivity.kt
+++ app/src/main/java/com/example/githubexample/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.githubexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
